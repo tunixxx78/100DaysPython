9ffc342748619eaa0e63f1a479bf1c8ead90c3d0
tunixxx78 <tunixxx@gmail.com>
2024-05-20 10:28:25 +0300

    bandNameGen.py

--- Day1/bandNameGen.py
+++ Day1/bandNameGen.py
@@ -1,7 +1,8 @@
-
+  
 print("This is Band Name Generator!")
 
 town = input("Give yor birth town's name: ")
 pet = input("Give your pet's name: ")
 
 print("Your band name is: " + town + pet)
+print("Your band name is: " + town + " " + pet)

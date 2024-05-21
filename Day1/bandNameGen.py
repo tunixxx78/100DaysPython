03fbd0c7d0a6fedcccaffd219c2c8422a2e5d331
tunixxx78 <tunixxx@gmail.com>
2024-05-21 11:18:14 +0300

    re-worked code

--- Day1/bandNameGen.py
+++ Day1/bandNameGen.py
@@ -1,8 +1,9 @@
   
 print("This is Band Name Generator!")
 
-town = input("Give yor birth town's name: ")
-pet = input("Give your pet's name: ")
+print("Your band name is: " + input("Give your home town's name: ") + " " + input("give your pet's name: "))
+#town = input("Give yor birth town's name: ")
+#pet = input("Give your pet's name: ")
 
-print("Your band name is: " + town + pet)
-print("Your band name is: " + town + " " + pet)
+#print("Your band name is: " + town + pet)
+#print("Your band name is: " + town + " " + pet)

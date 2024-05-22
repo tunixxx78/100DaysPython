90351a730c21486ac19ea34f669c9cbf7f8a5b11
tunixxx78 <tunixxx@gmail.com>
2024-05-22 09:47:18 +0300

    second commit to tip calc

--- Day2/TipCalc.py
+++ Day2/TipCalc.py
@@ -7,4 +7,5 @@ tip_amount = float(bill_amount) * (float(tip_amount) / 100)
 bill_with_tip = float(bill_amount) + tip_amount
 divided_bill = bill_with_tip / int(number_of_people)
 
+divided_bill = round(divided_bill, 2)
 print("Each will pay: " + str(divided_bill))
